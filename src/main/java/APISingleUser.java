e1085256ca9313e5847fc02f7bbf9d458ea6b9d8
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-16 13:22:47 +0300

    Added API respose for single user

--- src/main/java/APISingleUser.java
+++ src/main/java/APISingleUser.java
@@ -1,2 +1,31 @@
+import io.restassured.response.Response;
+import org.testng.Assert;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+
 public class APISingleUser {
+
+    void APISingleUserOne() {
+
+        Response response = get("https://reqres.in/api/users/2");
+
+        System.out.println("Response: " + response.asString());
+        System.out.println("Status Code: " + response.getStatusCode());
+        System.out.println("Body: " + response.getBody().asString());
+        System.out.println("Time taken: " + response.getTime());
+        System.out.println("Info: " + response.getBody());
+        System.out.println("Header: " + response.getHeader("content-type"));
+
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+
+    void APISingleUserTwo() {
+        given().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                statusCode(200);
+        System.out.println("Success!");
+    }
 }
