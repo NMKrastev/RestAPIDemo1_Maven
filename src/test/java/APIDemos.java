3d901932bcabd5a7d174c876c33f596d3243e681
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-16 11:12:32 +0300

    Created response using REST-Assured that prints several values

--- src/test/java/APIDemos.java
+++ src/test/java/APIDemos.java
@@ -1,3 +1,6 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class APIDemos {
@@ -5,5 +8,13 @@ public class APIDemos {
     @Test
     void demoOne() {
 
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+
+        System.out.println("Response: " + response.asString());
+        System.out.println("Status Code: " + response.getStatusCode());
+        System.out.println("Body: " + response.getBody().asString());
+        System.out.println("Time taken: " + response.getTime());
+        System.out.println("Header: " + response.getHeader("content-type"));
+        
     }
 }
