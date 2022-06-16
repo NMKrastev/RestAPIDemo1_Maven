af262528ab6d2aa5d1564c001169fbd1e2bf5edd
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-16 11:19:13 +0300

    Imported the whole package RestAssured

--- src/test/java/APIDemos.java
+++ src/test/java/APIDemos.java
@@ -1,4 +1,4 @@
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,14 +8,14 @@ public class APIDemos {
     @Test
     void demoOne() {
 
-        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        Response response = get("https://reqres.in/api/users?page=2");
 
         System.out.println("Response: " + response.asString());
         System.out.println("Status Code: " + response.getStatusCode());
         System.out.println("Body: " + response.getBody().asString());
         System.out.println("Time taken: " + response.getTime());
         System.out.println("Header: " + response.getHeader("content-type"));
-        
+
         int statusCode = response.statusCode();
         Assert.assertEquals(statusCode, 200);
     }
